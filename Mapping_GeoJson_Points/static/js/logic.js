27daafbbef0b97723c73b87c59d9e464a22a025c
COcampo95 <cocampo95@gmail.com>
2022-08-31 20:55:22 -0400

    15.3.4

--- Mapping_GeoJson_Points/static/js/logic.js
+++ Mapping_GeoJson_Points/static/js/logic.js
@@ -1,17 +1,40 @@
-// Create the map object with center and zoom level.
-let map = L.map('mapid').setView([30, 30], 2);
+// add console.log to see if our code is working
+console.log("working");
 
+// Having the tileLayer before accessing large datasets ensures that the map gets loaded before the data is added to it
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+// We create the dark view tile layer that will be an option for our map.
+let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
 });
 
-// Then we add our 'graymap' tile layer to the map.
-streets.addTo(map);
+// Still dont have the addTo(map)
+// instead, create a base layer that holds both maps
+let baseMaps = {
+    Street: streets,
+    Dark: dark
+}; 
 
-let airportData = "https://raw.githubusercontent.com/<GitHub_name>/Mapping_Earthquakes/main/majorAirports.json";
+// Create the map object with a center (US) and zoom level
+let map = L.map('mapid', {
+    center: [30, 30],
+    zoom: 2,
+    layers: [streets]
+});
+
+// Control layers 
+// Pass our map layers into our layers control and add the layers control to the map
+L.control.layers(baseMaps).addTo(map);
+
+let airportData = "https://raw.githubusercontent.com/COcampo95/Mapping_Earthquakes/Mapping_GeoJSON_Points/Mapping_GeoJson_Points/majorAirports.json"
 
 // Grabbing our GeoJSON data.
 d3.json(airportData).then(function(data) {
